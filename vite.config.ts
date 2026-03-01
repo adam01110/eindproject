cd371285a962b9c5ff7a880bf35b25ac99f9ec13
adam01110 <alumail@protonmail.com>
2026-03-01 01:00:57 +0100

    tabler

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
+import { tablerWoff2Only } from "./build/tablerWoff2Only";
 
 export default defineConfig({
 	root: "src",
+	plugins: [tablerWoff2Only()],
 	build: {
 		outDir: "../dist",
 		emptyOutDir: true,
